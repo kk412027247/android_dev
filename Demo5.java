38be09b48feb921203224293c5ceb9e7c9bbfde3
TMD <1@123.com>
2018-08-12 13:24:31 +0800

    Initial commit

---
+++ Demo5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Demo5{
+  public static void main(String args[]){
+    // char ch1 = '国',ch2 = '庆';
+    // System.out.println(ch1);
+    // System.out.println(ch2);
+    // System.out.println((int)ch1);
+    // System.out.println((int)ch2);
+    // int p1 =969, p2 = 12353;
+    // System.out.println((char)p1);
+    // System.out.println((char)p2);
+
+
+    // float weight = 82.5f;
+    // int height =  176;
+    // double d = (height -100)/weight;
+    //
+    // System.out.println(d);
+
+
+    // Scanner reader = new Scanner(System.in);
+    //
+    // System.out.println("input");
+    // int i = reader.nextInt();
+    // System.out.println(i);
+
+    int i =12;
+    System.out.printf("i is %d",i);
+
+
+  }
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+  public static void main(String args[]){
+    System.out.println("hello world");
+    char ch = 'A';
+    System.out.println(ch);
+  }
+}
+
